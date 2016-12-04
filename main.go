818f8ef89072a3e9017fb2bbb23b0fd01d5f7536
135yshr <isago@praisefirst.jp>
2016-12-04 13:23:53 +0900

    chg goroutine

--- main.go
+++ main.go
@@ -13,10 +13,25 @@ func main() {
 	rpio.Open()
 	pin := rpio.Pin(9)
 	pin.Input()
-	for {
-		res := pin.Read()
-		fmt.Println(res)
 
-		time.Sleep(1 * time.Second)
+	st := make(chan rpio.State)
+	go func(st chan rpio.State) {
+		old := rpio.High
+		for {
+			res := pin.Read()
+			if res != old {
+				st <- res
+				old = res
+			}
+
+			time.Sleep(1 * time.Second)
+		}
+	}(st)
+	for {
+		select {
+		case res := <-st:
+			fmt.Println(res)
+		default:
+		}
 	}
 }
