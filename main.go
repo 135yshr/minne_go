18a843c42e66c03e910c46c07775a0ceeb659394
135yshr <isago@praisefirst.jp>
2016-12-04 13:49:08 +0900

    add will not go to cable

--- main.go
+++ main.go
@@ -11,19 +11,29 @@ func main() {
 	fmt.Println("Hello, world!")
 
 	rpio.Open()
-	pin := rpio.Pin(9)
-	pin.Input()
+	to_go := rpio.Pin(9)
+	to_go.Input()
+	do_not_go := rpio.Pin(10)
+	do_not_go.Input()
 
-	st := make(chan rpio.State)
-	go waiting_pin(pin, st)
+	cg := make(chan rpio.State)
+	cn := make(chan rpio.State)
+	go waiting_pin(to_go, cg)
+	go waiting_pin(do_not_go, cn)
 	for {
 		select {
-		case res := <-st:
-			fmt.Println(res)
+		case res := <-cg:
+			if res == rpio.High {
+				fmt.Println("I'm going to izakaya.")
+			}
+		case res := <-cn:
+			if res == rpio.High {
+				fmt.Println("I will not go to izakaya.")
+			}
 		default:
 		}
 	}
-	close(st)
+	close(cg)
 }
 
 func waiting_pin(pin rpio.Pin, st chan rpio.State) {
